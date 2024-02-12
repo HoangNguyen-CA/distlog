252bd91790bef49954a266104647709342e5b643
HoangNguyen-CA <hoang.viet.nguyen@outlook.com>
2024-02-12 00:56:01 -0500

    added one-way TLS to encrypt data transfer

--- internal/server/server_test.go
+++ internal/server/server_test.go
@@ -7,9 +7,11 @@ import (
 	"testing"
 
 	api "github.com/HoangNguyen-CA/distlog/api/v1"
+	"github.com/HoangNguyen-CA/distlog/internal/config"
 	"github.com/HoangNguyen-CA/distlog/internal/log"
 	"github.com/stretchr/testify/require"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
 )
 
 func TestServer(t *testing.T) {
@@ -35,13 +37,30 @@ func setupTest(t *testing.T, fn func(*Config)) (
 	cfg *Config,
 	teardown func(),
 ) {
-
 	t.Helper()
-	l, err := net.Listen("tcp", ":0")
+	l, err := net.Listen("tcp", "127.0.0.1:0")
 	require.NoError(t, err)
-	clientOptions := []grpc.DialOption{grpc.WithInsecure()}
-	cc, err := grpc.Dial(l.Addr().String(), clientOptions...)
+	clientTLSConfig, err := config.SetupTLSConfig(config.TLSConfig{
+		CAFile: config.CAFile,
+	})
 	require.NoError(t, err)
+	clientCreds := credentials.NewTLS(clientTLSConfig)
+	cc, err := grpc.Dial(
+
+		l.Addr().String(),
+		grpc.WithTransportCredentials(clientCreds),
+	)
+	require.NoError(t, err)
+	client = api.NewLogClient(cc)
+
+	serverTLSConfig, err := config.SetupTLSConfig(config.TLSConfig{
+		CertFile:      config.ServerCertFile,
+		KeyFile:       config.ServerKeyFile,
+		CAFile:        config.CAFile,
+		ServerAddress: l.Addr().String(),
+	})
+	require.NoError(t, err)
+	serverCreds := credentials.NewTLS(serverTLSConfig)
 	dir, err := ioutil.TempDir("", "server-test")
 	require.NoError(t, err)
 	clog, err := log.NewLog(dir, log.Config{})
@@ -52,17 +71,15 @@ func setupTest(t *testing.T, fn func(*Config)) (
 	if fn != nil {
 		fn(cfg)
 	}
-	server, err := NewGRPCServer(cfg)
+	server, err := NewGRPCServer(cfg, grpc.Creds(serverCreds))
 	require.NoError(t, err)
 	go func() {
 		server.Serve(l)
 	}()
-	client = api.NewLogClient(cc)
 	return client, cfg, func() {
 		server.Stop()
 		cc.Close()
 		l.Close()
-		clog.Remove()
 	}
 }
 
